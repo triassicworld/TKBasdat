26095a3ab8f17f0a74162c77d7ddb7605df1b65a
hamdanpapeng <hamdanpapeng@gmail.com>
2016-05-18 00:47:13 +0700

    Update lihat_pemesanan_makanan.php

--- lihat_pemesanan_makanan.php
+++ lihat_pemesanan_makanan.php
@@ -1,5 +1,14 @@
 <?php
     include('getNama.php');
+	date_default_timezone_set('Asia/Jakarta');
+	$date = date('Y-m-d', time());
+	$date1 = "";
+	$date2 = "";
+	 if(isset($_GET["tanggal"])) {
+		$date = $_GET["tanggal"];
+		$date1 = "'".$date." 00:00:01'";
+		$date2 = "'".$date." 23:59:59'";
+	 }
 ?>
 
 <!DOCTYPE html>
@@ -18,48 +27,72 @@
 
 <body>
 	<nav class="navbar navbar-default">
-	  <div class="container-fluid">
-	    <!-- Brand and toggle get grouped for better mobile display -->
-	    <div class="navbar-header">
-	      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-	        <span class="sr-only">Toggle navigation</span>
-	        <span class="icon-bar"></span>
-	        <span class="icon-bar"></span>
-	        <span class="icon-bar"></span>
-	      </button>
-	      <a class="navbar-brand" href="home_kasir.php">TK Basdat</a>
-	    </div>
+      <div class="container-fluid">
+        <!-- Mobile display -->
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="home_kasir.php">TK Basdat</a>
+        </div>
 
-	    <!-- Collect the nav links, forms, and other content for toggling -->
-	    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-	      <ul class="nav navbar-nav navbar-right">
-	        <li><a href="#">Home </a></li>
-	        <li><a href="#">Lihat Menu </a></li>
-	        <li class="active"><a href="#">Lihat Pemesanan Makanan </a></li>
-	        <li class="dropdown">
-	          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
-  		          <?php echo "<span style='color:blue'>".$nama."</span>"; ?>
-	    	      <span class="caret"></span>
-	          </a>
-	          <ul class="dropdown-menu">
-	            <li><a href="#">Profile</a></li>
-	            <li><a href="#">Setting</a></li>
-	            <li role="separator" class="divider"></li>
-	            <li><a href="logout.php">Log Out</a></li>
-	          </ul>
-	        </li>
-	      </ul>
-	    </div><!-- /.navbar-collapse -->
-	  </div><!-- /.container-fluid -->
-	</nav>
+        <!-- Navbar links -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+          <ul class="nav navbar-nav navbar-right">
+            <li class="active"><a href="home.php">Home </a></li>
+			<?php
+			if($yeay == "Chef") {
+				echo "<li>"."<a href='lihat_menu.php'>"."Lihat Menu"."</a>"."</li>";
+			}
+			else if($yeay == "Kasir") {
+				echo "<li>"."<a href='lihat_menu.php'>"."Lihat Menu"."</a>"."</li>";
+				echo "<li>"."<a href='lihat_pemesanan_makanan.php'>"."Lihat Pemesanan Makanan"."</a>"."</li>";
+			}
+			else if($yeay == "Staf") {
+				echo "<li>"."<a href='lihat_pembelian_bahan_makanan.php'>"."Lihat Pembelian Bahan Makanan"."</a>"."</li>";
+				echo "<li>"."<a href='beli_bahan_makanan.php'>"."Beli Bahan Makanan"."</a>"."</li>";
+			}
+			else {
+				echo "";
+			}
+			?>
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+                      <?php echo "<span style='color:blue'>".$nama."</span>"; ?>
+                  <span class="caret"></span>
+              </a>
+              <ul class="dropdown-menu">
+                <li><a href="#">Profile</a></li>
+                <li><a href="#">Setting</a></li>
+                <li role="separator" class="divider"></li>
+                <li><a href="logout.php">Log Out</a></li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
 
     <!-- Welcoming -->
     <div id="login" class="container">
         <div class="row">
             <div class="col-lg-4 col-lg-offset-4 col-md-10 col-md-offset-1 text-center">
                 <div class="well">
-                    <h3>Hello, kasir 
-                        <?php echo " <span style='color:blue'>".$nama."</span>"; ?>
+                    <h3>Hello, 
+                        <?php 
+						$yeay = "";
+						if($role == "KS")
+							$yeay = "Kasir";
+						else if($role == "CH")
+							$yeay = "Chef";
+						else if($role == "ST")
+							$yeay = "Staf";
+						else
+							$yeay = "Manager";
+						echo " <span style='color:blue'>".$yeay." ".$nama."</span>"; ?>
                     </h3>
                 </div>
             </div>
@@ -71,6 +104,12 @@
             <hr>
             <div class="col-lg-4 col-lg-offset-4 col-md-10 col-md-offset-1 text-center">
                 <!-- <div class="well"> -->
+				<div>
+					<form method="GET" action="lihat_pemesanan_makanan.php">
+					<input name = 'tanggal' type = 'date' value = 'tes'></input>
+					<input type = 'submit'></input> 
+					</form>
+				</div>
                 <?php 
                     $hostname = "localhost";
                     $user = "root";
@@ -78,40 +117,45 @@
                     $database = "foodie";
 
                     //Loadmore configuarion
-                    $resultsPerPage = 10;
+                    $resultsPerPage = 15;
                     $bd = mysql_connect($hostname, $user, $password) or die("Failed to connect to database");
-                    mysql_select_db($database, $bd) or die("Database Not Found");
+					if(isset($_GET["tanggal"])){
+						mysql_select_db($database, $bd) or die("Database Not Found");
 
-                    $que=mysql_query("SELECT * FROM `PEMESANAN` ORDER BY `WAKTUPESAN` DESC");
-                    
-                    if($que === FALSE) { 
-                        die(mysql_error()); 
-                    }
-                    
-                    $count = 0;
-                    while ($count < $resultsPerPage && $data = mysql_fetch_array($que)) {
-                        // if ($data['EMAILKASIR'] == $emailSession) {
-                            echo "<div class='well'>";
-                            echo "<strong>".($count+1)."</strong>";
-                            echo "<hr>";
-                            echo "Nomor Nota : <span style='color:red'>".$data['NOMORNOTA']."</span>";
-                            echo "<br>Waktu Pesan : <strong>".$data['WAKTUPESAN']."</strong>";
-                            echo "<br>Waktu Bayar : <strong>".$data['WAKTUBAYAR']."</strong>";
-                            echo "<br>Total : <strong>Rp ".$data['TOTAL'].",-</strong>";
-                            echo "<br><br>Email kasir : ".$data['EMAILKASIR'];
-                            echo "<br>Mode bayar : ".$data['MODE'];
-                            echo "</div>";
-                            $count++;
-                        // }
-                    }
+						$que=mysql_query("SELECT * FROM `PEMESANAN` WHERE WAKTUPESAN BETWEEN $date1 AND $date2 ORDER BY `WAKTUPESAN` DESC");
+						
+						if($que === FALSE) { 
+							die(mysql_error()); 
+						}
+						
+						$count = 0;
+						while ($count < $resultsPerPage && $data = mysql_fetch_array($que)) {
+							// if ($data['EMAILKASIR'] == $emailSession) {
+								echo "<div class='well'>";
+								echo "<strong>".($count+1)."</strong>";
+								echo "<hr>";
+								echo "Nomor Nota : <span style='color:red'>".$data['NOMORNOTA']."</span>";
+								echo "<br>Waktu Pesan : <strong>".$data['WAKTUPESAN']."</strong>";
+								echo "<br>Waktu Bayar : <strong>".$data['WAKTUBAYAR']."</strong>";
+								echo "<br>Total : <strong>Rp ".$data['TOTAL'].",-</strong>";
+								echo "<br>Email kasir : ".$data['EMAILKASIR'];
+								echo "<br>Mode bayar : ".$data['MODE'];
+								echo "<br><a href = 'rincian_pemesanan_makanan.php'>Rincian</a>";
+								echo "</div>";
+								
+								$count++;
+							// }
+						}
+					}
                 ?>
                 <!-- </div> -->
             </div>
         </div>
     </div>
+	<center><button>Next Page</button></center>
 
     <!-- Footer -->
-    <<?php include('footer.php'); ?>
+    <?php include('footer.php'); ?>
 
     <!-- jQuery -->
     <script src="js/jquery.min.js"></script>
