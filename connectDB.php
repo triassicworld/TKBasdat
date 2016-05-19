e5fd1fb7c47c388a3c019df0d5084268a813bf33
triassicworld <triassicworld@gmail.com>
2016-05-19 16:43:40 +0700

    konek ke postgre

--- connectDB.php
+++ connectDB.php
@@ -1,15 +1,17 @@
 <?php
 	function connectDB() {
         $servername = "localhost";
-        $username = "root";
-        $password = "";
+        $username = "postgres";
+        $password = "asdasdasd";
         $dbName = "foodie";
 
         //Create connection
-        $conn = mysqli_connect($servername, $username, $password, $dbName);
+        // $conn = mysqli_connect($servername, $username, $password, $dbName);
+        $conn = pg_connect("host=localhost port=5432 dbname=postgres user=postgres password=asdasdasd");
 
         if(!$conn) {
-            die("Connection failed: ".mysqli_connect_error());
+            // die("Connection failed: ".mysqli_connect_error());
+            die("Connection failed: ");
         }
 
         return $conn;
