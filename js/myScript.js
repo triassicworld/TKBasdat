04ad69e83b0754bc519e32c22aaf2ec972a483b0
triassicworld <triassicworld@gmail.com>
2016-05-22 03:52:41 +0700

    teletubbies

--- js/myScript.js
+++ js/myScript.js
@@ -1,28 +1,24 @@
 $(document).ready(function() {
 	
-	var Arrays=new Array();
+	var Arrays = new Array();
+	var Rincian = new Array();
 	
 	$('#wrap li').mousemove(function(){
 		
 		var position = $(this).position();
 		
-		$('#cart').stop().animate({
-																									
+		$('#cart').stop().animate({															
 				left   : position.left+'px',
-				
 			},250,function(){
-			
 		});			
-	}).mouseout(function(){
-		
-	});	
+	}).mouseout(function(){});	
 	
 	$('#wrap li').click(function(){
-		
 		var thisID = $(this).attr('id');
 		
 		var itemname  = $(this).find('div .namaBahan').html();
 		var itemprice = $(this).find('div .price').html();
+		var itemunit = $(this).find('div .satuanstok').html();
 			
 		if(include(Arrays,thisID))
 		{
@@ -32,26 +28,34 @@ $(document).ready(function() {
 			
 			var total = parseInt(itemprice)*parseInt(quantity);
 			
+			//Update rincian
+			Rincian[thisID][4] += 1;
+			Rincian[thisID][5] = total;
+
 			$('#each-'+thisID).children(".shopp-price").find('em').html(total);
 			$('#each-'+thisID).children(".shopp-quantity").html(quantity);
 			
 			var prev_charges = $('.cart-total span').html();
 			prev_charges = parseInt(prev_charges)-parseInt(price);
-			
 			prev_charges = parseInt(prev_charges)+parseInt(total);
+
 			$('.cart-total span').html(prev_charges);
-			
+
 			$('#total-hidden-charges').val(prev_charges);
+			$('#rincian-hidden').val(Rincian);
 		}
 		else
 		{
 			Arrays.push(thisID);
+			Rincian.push(new Array(thisID, itemname, itemprice, itemunit, 1, itemprice));
 			
 			var prev_charges = $('.cart-total span').html();
 			prev_charges = parseInt(prev_charges)+parseInt(itemprice);
 			
 			$('.cart-total span').html(prev_charges);
+
 			$('#total-hidden-charges').val(prev_charges);
+			$('#rincian-hidden').val(Rincian);
 			
 			$('#left_bar .cart-info').append('<div class="shopp" id="each-'+thisID+'"><div class="label">'+itemname+'</div><div class="shopp-price"> Rp <em>'+itemprice+'</em></div><span class="shopp-quantity">1</span><img src="img/remove.ico" class="remove" /><br class="all" /></div>');
 			
@@ -63,32 +67,28 @@ $(document).ready(function() {
 	
 	
 	$('.remove').livequery('click', function() {
-		
 		var deduct = $(this).parent().children(".shopp-price").find('em').html();
 		var prev_charges = $('.cart-total span').html();
-		
 		var thisID = $(this).parent().attr('id').replace('each-','');
-		
+
 		var pos = getpos(Arrays,thisID);
-		Arrays.splice(pos,1,"0")
+		var pos2 = getpos(Rincian,thisID);
+		Arrays.splice(pos,1,"0");
+		Rincian.splice(pos2,1,"0");
 		
 		prev_charges = parseInt(prev_charges)-parseInt(deduct);
 		$('.cart-total span').html(prev_charges);
 		$('#total-hidden-charges').val(prev_charges);
+		$('#rincian-hidden').val(Rincian);
 		$(this).parent().remove();
-		
-	});	
+	});
 	
 	$('#Submit').livequery('click', function() {
-		
-		var totalCharge = $('#total-hidden-charges').val();
-		
-		$('#left_bar').html('Total Charges: $'+totalCharge);
-		
-		return false;
-		
-	});	
-	
+		// menghilangkan list dan menampilkan harga total
+		// var totalCharge = $('#total-hidden-charges').val();
+		// $('#left_bar').html('Total Charges: $'+totalCharge); 
+		return true;
+	});
 });
 
 function include(arr, obj) {
@@ -96,9 +96,13 @@ function include(arr, obj) {
     if (arr[i] == obj) return true;
   }
 }
+
 function getpos(arr, obj) {
   for(var i=0; i<arr.length; i++) {
     if (arr[i] == obj) return i;
   }
 }
-function angle(){$('#cart').css({'-webkit-transform' : 'rotate(0deg)','-moz-transform' : 'rotate(0deg)' });}
+
+function angle() {
+	$('#cart').css({'-webkit-transform' : 'rotate(0deg)','-moz-transform' : 'rotate(0deg)' });
+}
