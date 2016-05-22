ec84cbe9b234069c61e1e18e5c4b2cb5b8e06973
fauzandiMB <fauzandi_12@yahoo.co.id>
2016-05-22 09:34:00 +0700

    Update rincian_pemesanan_makanan.php

--- rincian_pemesanan_makanan.php
+++ rincian_pemesanan_makanan.php
@@ -3,38 +3,87 @@
     include('headAndNavbar.php');
 ?>
 
-	<center>
-		<h3>FOODIE - RINCIAN PEMESANAN MAKANAN</h3>
-		<h5>Nomor Nota: </h5>
-		<h5>Waktu Pesan: </h5>
-		<h5>Waktu Bayar: </h5>
-		<h5>Total:</h5>
-		<h5>Kasir: </h5>
-		<h5>Mode Bayar:</h5>
-		
-		<hr>
-		<hr>
-		
-		<table width="80%">
-			<tr>
-				<td><Strong>Nama Menu</Strong></td>
-				<td><Strong>Harga</Strong></td>
-				<td><Strong>Jumlah</Strong></td>
-				<td><Strong>Total</Strong></td>				
-			</tr>
-				<td>Dummy</td>
-				<td>Dummy</td>
-				<td>Dummy</td>
-				<td>Dummy</td>
-			<tr>
-			</tr>
-			<tr>
-			</tr>
-			<tr>
-			</tr>
-		</table>
-	</center>
+<?php
+    $conn = connectDB();
+
+    $sql = "SELECT * FROM foodie.pemesanan_menu_harian WHERE nomornota='".$_GET["nomornota"]."' ORDER BY namamenu ASC";
+    $sql2 = "SELECT * FROM foodie.pemesanan WHERE nomornota='".$_GET["nomornota"]."'";
+    $sql3 = "SELECT * FROM foodie.menu, foodie.pemesanan, foodie.pemesanan_menu_harian WHERE menu.nama = pemesanan_menu_harian.namamenu AND pemesanan.nomornota = pemesanan_menu_harian.nomornota AND pemesanan.nomornota='".$_GET["nomornota"]."'";
+    //$sql2 = "SELECT * FROM foodie.kasir, foodie.pemesanan WHERE email=emailkasir AND "
+
+    $goExec = $conn->prepare($sql);
+    $goExec->execute();
+
+    $goExec2 = $conn->prepare($sql2);
+    $goExec2->execute();
+    
+
+    $bb = "";
+    $cc = "";
+    $dd = "";
+    $id = 1;
+
+
+    while($cc = $goExec2->fetch()) {
+    	echo '<center><h4><strong>FOODIE RINCIAN PEMESANAN</strong></h4></center>';
+	    echo '<hr>';
+
+	    echo '<div id="" class="container">
+	        	<div class="row">
+	        	<table class="table">
+	            <thead>
+	            	<tr>
+		                <th>Nomor Nota  : '.$cc['nomornota'].'</th>
+		                <th>Total: '.$cc['total'].'</th>
+	            	</tr>
+	            	<tr>
+		                <th>Waktu Pesan: '.$cc['waktupesan'].'</th>
+		                <th>Kasir: '.$cc['emailkasir'].'</th>
+	                </tr>
+	            	<tr>
+	                	<th>Waktu Bayar : '.$cc['waktubayar'].'</th>
+	                	<th>Mode Pembayaran: '.$cc['mode'].'</th>
+	              	</tr>
+	            </thead>';
+    }
+    
+    echo '	<div id="" class="container">
+        	<div class="row">
+        	<table class="table">
+            <thead>
+              <tr>
+                <th>Nama Menu</th>
+                <th>Harga</th>
+                <th>Jumlah</th>
+                <th>Total</th>
+              </tr>
+            </thead><tbody>';
+
+    $count = 0;
+    while(($bb = $goExec->fetch()) && $count < 15) {
+        $harga = "";
+        $goExec3 = $conn->prepare($sql3);
+    	$goExec3->execute();
+        while($dd = $goExec3->fetch()) {
+    		$harga = $dd["harga"];
+    	}
+        echo'<tr>
+                <td>'.$bb['namamenu'].'</td>
+                <td>'.$harga.'</td>
+                <td>'.$bb['jumlah'].'</td>
+                <td>'.($harga*$bb['jumlah']).'</td>
+            </tr>
+          ';
+          $count++;
+    }
+    echo '<tbody></table>';
+
+    if($count>15) {
+        echo '<center><button>Next Page</button></center>';
+    }
+    echo ' <hr></div></div>';
+?>
 	
 <?php
-    include('headAndNavbar.php');
+    include('footer.php');
 ?>
